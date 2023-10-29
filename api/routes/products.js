6ef4dd3d702c8b4440ffd94f1b98f21a686ff300
Jasmine <jasminecharles@hotmail.com>
2023-10-29 14:11:31 -0700

    added routes for productID

--- api/routes/products.js
+++ api/routes/products.js
@@ -13,4 +13,37 @@ router.post('/', (req, res, next) => {
     })
 });
 
+
+router.get('/:productID', (req, res, next) => {
+    const id = req.params.productID;
+    if (id === 'specific') {
+        res.status(200).json({
+            message: 'This is a specific id',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID',
+            id: req.params.productID
+        });
+    }
+    });
+    
+
+    router.patch('/:productID', (req, res, next) => {
+        res.status(200).json({
+            message: 'Updated/modified product'
+        })
+    });
+
+
+    router.delete('/:productID', (req, res, next) => {
+        res.status(200).json({
+            message: 'Deleted product!'
+        })
+    });
+
+
+
+
 module.exports = router;
