220410a4656fe67ffbb2ca7a86ae1794491b2a82
Jasmine <jasminecharles@hotmail.com>
2023-10-28 22:43:39 -0700

    added api folder and products.js file

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works!'
-    });
-});
+const productRoutes = require('./api/routes/products');
+
+
+// app.use((req, res, next) => {
+//     res.status(200).json({
+//         message: 'It works!'
+//     });
+// });
+
+app.use('/products', productRoutes);
 
 module.exports = app;
